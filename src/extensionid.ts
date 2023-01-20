55c5d6918644d4657f551843ad5186ccec78fed1
Brad Johnson <bradsk88@gmail.com>
2023-01-19 21:09:09 -0600

    Send name and colors during registration

--- src/extensionid.ts
+++ src/extensionid.ts
@@ -1,3 +1,6 @@
 export const autoRunStartURL = 'https://personal.affinitycu.ca/Accounts/Summary';
+export const extensionBankName = 'Affinity Credit Union';
+export const extensionPrimaryColorHex = '96c93d';
+export const extensionSecondaryColorHex = '0f2c52';
 export const extensionId = 'hnlpdjeildmnlkflckcbajebfagmolhe';
 export const hubExtensionId = 'kcghjdbnjdaabfigmmjgmgiaehjidpoh';
